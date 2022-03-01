a0080e94ce2c65a964e92df860e3f97f196f7f87
ehubert17 <hubert.52@buckeyemail.osu.edu>
2022-02-28 19:37:49 -0500

    undo comments test

--- scrapers.py
+++ scrapers.py
@@ -2,8 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import json
 
-# test comment
-# test comment 2
 def getDriver(url):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
