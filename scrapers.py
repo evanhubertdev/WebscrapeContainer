1956d43dea7bff032a973a8d63add2eeb3386485
ehubert17 <hubert.52@buckeyemail.osu.edu>
2022-03-02 01:22:00 -0500

    removed 'electrified' in response to correct json formatting for bot

--- scrapers.py
+++ scrapers.py
@@ -31,15 +31,13 @@ def scrapeCars():
     minivanTruckData['minivan-truck'] = []
     suvData = {}
     suvData['crossover-and-suv'] = []
-    electricData = {}
-    electricData['electrified'] = []
 
     length = len(json_object['Models'])
     i = 0
 
     while i < length:
         
-        if (json_object['Models'][i]['Category']['Id'] == "sedans"):
+        if (json_object['Models'][i]['Category']['Id'] == "sedans" or json_object['Models'][i]['Category']['Id'] == "environmental-vehicles"):
             tempDict['name'] = json_object['Models'][i]['ModelName']
             tempDict['price'] = json_object['Models'][i]['Msrp']
             tempDict['mileage'] = json_object['Models'][i]['Mpg']
@@ -66,20 +64,12 @@ def scrapeCars():
             tempDict['mileage'] = json_object['Models'][i]['Mpg']
             tempDict['url'] = 'https://automobiles.honda.com/' + json_object['Models'][i]['ModelName'].replace(' ', '-').lower()
             minivanTruckData['minivan-truck'].append(tempDict)
-        elif (json_object['Models'][i]['Category']['Id'] == "environmental-vehicles"):
-            tempDict['name'] = json_object['Models'][i]['ModelName']
-            tempDict['price'] = json_object['Models'][i]['Msrp']
-            tempDict['mileage'] = json_object['Models'][i]['Mpg']
-            tempDict['url'] = 'https://automobiles.honda.com/' + json_object['Models'][i]['ModelName'].replace(' ', '-').lower()
-            electricData['electrified'].append(tempDict)
-            tempDict = {}
         i+=1
 
     formattedData['vehicles'].append(sedanData)
     formattedData['vehicles'].append(hatchbackData)
     formattedData['vehicles'].append(minivanTruckData)
     formattedData['vehicles'].append(suvData)
-    formattedData['vehicles'].append(electricData)
     return json.dumps(formattedData)
 
 """
